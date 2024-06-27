da094c38f654562fb518c8b69a2f70ea207fc918
DaniSurname <daniharvey25@gmail.com>
2024-06-27 22:37:36 +0100

    Replaced buttons on Scent page with a dropdown menu

--- src/scent-script.js
+++ src/scent-script.js
@@ -1,22 +1,29 @@
 function openName() {
+  // Sends user to Name page
   window.location.href = "name.html";
 }
 
 function saveQuery() {
+  // Collects the ID of the 🔍 button clicked
   let query = event.srcElement.id;
+  // Records ID in local storage
   window.localStorage.setItem("savedQuery", query);
+  // Triggers switch page functin
   openName();
 }
 
 function displayScentResults(list, query) {
+  // Sets variable to collect HTML code in
   let resultHtml = "";
 
   list.forEach(function (item) {
+    // Sets variables for each data type for each filtered array item
     let name = item.name.toUpperCase();
     let brand = item.brand;
     let scent = item.scent;
     let longevity = item.longevity;
 
+    // Constructs HTML to display for each filtered array item
     resultHtml =
       resultHtml +
       `<div class="list-result-container">
@@ -36,18 +43,42 @@ function displayScentResults(list, query) {
     </div>`;
   });
 
+  // Updates page with collect HTML for filtered results
   let resultElement = document.querySelector("#results");
   resultElement.innerHTML = resultHtml;
+  // Updates counter element with filtered list length
   let counterElement = document.querySelector("#counter");
   counterElement.innerHTML = `${list.length} result(s) found for ${query}:`;
 }
 
 function filterPerfumes(perfumes, query) {
+  // Filters 'perfumes' array by scent query
   let list = perfumes.filter((element) => element.scent.includes(query));
+  // Sends results to display function
   displayScentResults(list, query);
 }
 
+function toggleDropdown() {
+  // Toggles between dropdown arrow's open/closed styling
+  let dropdownArrow = document.querySelector(".arrow");
+  dropdownArrow.classList.toggle("arrow-rotate");
+  // Toggles between dropdown button's open/closed styling
+  let dropdownButton = document.querySelector(".dropdown-button");
+  dropdownButton.classList.toggle("menu-open");
+  // Toggles between dropdown menu's open/closed styling
+  let dropdownContent = document.querySelector(".dropdown-content");
+  dropdownContent.classList.toggle("menu-open");
+}
+
 function getScent() {
+  // Closes dropdown menu before proceeding
+  toggleDropdown();
+  // Records chosen scent as 'query'
   let query = event.srcElement.id;
+  // Sends query to filter function
   filterPerfumes(perfumes, query);
 }
+
+// Locates dropdown menu button and triggers open/close function
+let dropdownButton = document.querySelector(".dropdown-button");
+dropdownButton.addEventListener("click", toggleDropdown);
