764c632e8aeab8b251b6a61fe08e99ca6a4173d3
DaniSurname <daniharvey25@gmail.com>
2024-06-27 22:49:55 +0100

    Restructured files for clarity

--- src/wip-script.js
+++
@@ -1,9 +0,0 @@
-let dropdownButton = document.querySelector(".dropdown-button");
-let dropdownArrow = document.querySelector(".arrow");
-let dropdownContent = document.querySelector(".dropdown-content");
-
-dropdownButton.addEventListener("click", () => {
-  dropdownArrow.classList.toggle("arrow-rotate");
-  dropdownButton.classList.toggle("menu-open");
-  dropdownContent.classList.toggle("menu-open");
-});
