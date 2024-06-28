ca1580e53f8fc8a09d7f2752d3cc19dc6fe6cfe4
DaniSurname <daniharvey25@gmail.com>
2024-06-28 22:55:59 +0100

    Improved Name search functionality

--- src/scripts/name-script.js
+++ src/scripts/name-script.js
@@ -1,39 +1,80 @@
 function updatePerfumeInfo(index) {
-  // event.preventDefault();
-
-  let perfumeName = document.querySelector("#perfume-name");
-  let name = perfumes[index].name;
-  perfumeName.innerHTML = name.toUpperCase();
-
-  let perfumeBrand = document.querySelector("#perfume-brand");
+  let name = perfumes[index].name.toUpperCase();
   let brand = perfumes[index].brand;
-  perfumeBrand.innerHTML = `by ${brand}`;
-
-  let perfumeType = document.querySelector("#perfume-type");
   let type = perfumes[index].type;
-  perfumeType.innerHTML = type.toUpperCase();
-
-  let perfumeScent = document.querySelector("#perfume-scent");
   let scent = perfumes[index].scent;
-  perfumeScent.innerHTML = scent;
-
-  let perfumeLongevity = document.querySelector("#perfume-longevity");
   let longevity = perfumes[index].longevity;
-  perfumeLongevity.innerHTML = `<em>An ${type} has an expected longevity of approx. ${longevity}.</em>`;
-
-  // let perfumeImpression = document.querySelector("#perfume-impression");
-  // let impression = perfumes[index].impression;
-  // perfumeImpression.innerHTML = `<em>${impression}</em>`;
-
-  let perfumeSize = document.querySelector("#perfume-size");
   let size = perfumes[index].size;
-  perfumeSize.innerHTML = `${size} size in collection.`;
+  let impression = perfumes[index].impression;
 
-  let GoogleButton = document.querySelector("#google-button");
-  GoogleButton.innerHTML = `<a href="https://google.com/search?q=
-  ${perfumes[index].name} by ${perfumes[index].brand}">
-  Search on Google</a>`;
+  // Updates page with query result
+  let resultElement = document.querySelector(".name-result-container");
+  resultElement.innerHTML = `
+          <div class="name-result-sub-container">
+          <div class="title-text">${name}</div>
+          <div class="mid-text">by ${brand}</div>
+          <br />
+          <div class="caption-text">${type.toUpperCase()}</div>
+          <br />
+          <div class="caption-text"><em>Expected longevity: <br / >${longevity}</em></div>
+        </div>
+        <hr />
+        <div class="name-result-sub-container">
+          <div class="mid-text">${scent}</div>
+          <br />
+          <div><em>${impression}</em></div>
+          </div>
+          <hr />
+          <div class="name-result-sub-container">
+          <div>${size} size in collection.</div>
+          <a href="https://google.com">
+            <button id="google-button">
+            <a href="https://google.com/search?q=${perfumes[index].name} by ${
+    perfumes[index].brand
+  }">
+            Search on Google</a></button>
+          </a>
+        </div>
+        </div>
+`;
 }
+
+// function updatePerfumeInfo(index) {
+//   // event.preventDefault();
+
+//   let perfumeName = document.querySelector("#perfume-name");
+//   let name = perfumes[index].name;
+//   perfumeName.innerHTML = name.toUpperCase();
+
+//   let perfumeBrand = document.querySelector("#perfume-brand");
+//   let brand = perfumes[index].brand;
+//   perfumeBrand.innerHTML = `by ${brand}`;
+
+//   let perfumeType = document.querySelector("#perfume-type");
+//   let type = perfumes[index].type;
+//   perfumeType.innerHTML = type.toUpperCase();
+
+//   let perfumeScent = document.querySelector("#perfume-scent");
+//   let scent = perfumes[index].scent;
+//   perfumeScent.innerHTML = scent;
+
+//   let perfumeLongevity = document.querySelector("#perfume-longevity");
+//   let longevity = perfumes[index].longevity;
+//   perfumeLongevity.innerHTML = `<em>An ${type} has an expected longevity of approx. ${longevity}.</em>`;
+
+//   // let perfumeImpression = document.querySelector("#perfume-impression");
+//   // let impression = perfumes[index].impression;
+//   // perfumeImpression.innerHTML = `<em>${impression}</em>`;
+
+//   let perfumeSize = document.querySelector("#perfume-size");
+//   let size = perfumes[index].size;
+//   perfumeSize.innerHTML = `${size} size in collection.`;
+
+//   let GoogleButton = document.querySelector("#google-button");
+//   GoogleButton.innerHTML = `<a href="https://google.com/search?q=
+//   ${perfumes[index].name} by ${perfumes[index].brand}">
+//   Search on Google</a>`;
+// }
 
 function getPerfumeID(perfumeFinder) {
   let index = perfumes.findIndex((item) => item.finder === perfumeFinder);
